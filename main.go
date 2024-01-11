2c7c48db7b678748bee7553fc603a98fbccb739b
RioIglesias <rioiglesias03@gmail.com>
2024-01-11 11:52:01 +0700

    refactor code

--- main.go
+++ main.go
@@ -1,125 +1,16 @@
 package main
 
 import (
+	"ManganKu_BE/controllers/router"
+	"ManganKu_BE/database"
+	"ManganKu_BE/database/migration"
 	"fmt"
 	"log"
-	"net/http"
 	"os"
-	"testdb/models"
-	"testdb/storage"
 
 	"github.com/gofiber/fiber/v2"
-	"github.com/joho/godotenv"
 	"gorm.io/gorm"
 )
-
-type Book struct {
-	Author    string `json:"author"`
-	Title     string `json:"title"`
-	Publisher string `json:"publisher"`
-}
-
-type Repository struct {
-	DB *gorm.DB
-}
-
-func (r *Repository) CreateBook(context *fiber.Ctx) error {
-	book := Book{}
-
-	err := context.BodyParser(&book)
-
-	if err != nil {
-		context.Status(http.StatusUnprocessableEntity).JSON(
-			&fiber.Map{"message": "request failed"})
-		return err
-	}
-
-	err = r.DB.Create(&book).Error
-	if err != nil {
-		context.Status(http.StatusBadRequest).JSON(
-			&fiber.Map{"message": "could not create book"})
-		return err
-	}
-
-	context.Status(http.StatusOK).JSON(&fiber.Map{
-		"message": "book has been added"})
-	return nil
-}
-
-func (r *Repository) DeleteBook(context *fiber.Ctx) error {
-	bookModel := models.Books{}
-	id := context.Params("id")
-	if id == "" {
-		context.Status(http.StatusInternalServerError).JSON(&fiber.Map{
-			"message": "id cannot be empty",
-		})
-		return nil
-	}
-
-	err := r.DB.Delete(bookModel, id)
-
-	if err.Error != nil {
-		context.Status(http.StatusBadRequest).JSON(&fiber.Map{
-			"message": "could not delete book",
-		})
-		return err.Error
-	}
-	context.Status(http.StatusOK).JSON(&fiber.Map{
-		"message": "book delete successfully",
-	})
-	return nil
-}
-
-func (r *Repository) GetBooks(context *fiber.Ctx) error {
-	bookModels := &[]models.Books{}
-
-	err := r.DB.Find(bookModels).Error
-	if err != nil {
-		context.Status(http.StatusBadRequest).JSON(
-			&fiber.Map{"message": "could not get books"})
-		return err
-	}
-
-	context.Status(http.StatusOK).JSON(&fiber.Map{
-		"message": "books fetched successfully",
-		"data":    bookModels,
-	})
-	return nil
-}
-
-func (r *Repository) GetBookByID(context *fiber.Ctx) error {
-
-	id := context.Params("id")
-	bookModel := &models.Books{}
-	if id == "" {
-		context.Status(http.StatusInternalServerError).JSON(&fiber.Map{
-			"message": "id cannot be empty",
-		})
-		return nil
-	}
-
-	fmt.Println("the ID is", id)
-
-	err := r.DB.Where("id = ?", id).First(bookModel).Error
-	if err != nil {
-		context.Status(http.StatusBadRequest).JSON(
-			&fiber.Map{"message": "could not get the book"})
-		return err
-	}
-	context.Status(http.StatusOK).JSON(&fiber.Map{
-		"message": "book id fetched successfully",
-		"data":    bookModel,
-	})
-	return nil
-}
-
-func (r *Repository) SetupRoutes(app *fiber.App) {
-	api := app.Group("/api")
-	api.Post("/create_books", r.CreateBook)
-	api.Delete("delete_book/:id", r.DeleteBook)
-	api.Get("/get_books/:id", r.GetBookByID)
-	api.Get("/books", r.GetBooks)
-}
 
 func CloseDB(db *gorm.DB) {
 	sqlDB, err := db.DB()
@@ -137,38 +28,17 @@ func main() {
 		port = "8080"
 		log.Printf("defaulting to port %s", port)
 	}
-	mustGetenv := func(k string) string {
-		v := os.Getenv(k)
-		if v == "" {
-			log.Fatalf("Fatal Error in connect_unix.go: %s environment variable not set.\n", k)
-		}
-		return v
-	}
-	err := godotenv.Load(".env")
-	if err != nil {
-		log.Fatal(err)
-	}
-	config := &storage.Config{
-		Password: mustGetenv("DB_PASS"),
-		User:     mustGetenv("DB_USER"),
-		DBName:   mustGetenv("DB_NAME"),
-		DBHost:   mustGetenv("DB_HOST"),
-	}
+	// Initial Database Connection
+	database.DatabaseConnection()
+	defer CloseDB(database.DB)
 
-	db, err := storage.NewConnection(config)
-	if err != nil {
-		log.Fatal("could not load the database")
-	}
-	defer CloseDB(db)
-	err = models.MigrateBooks(db)
-	if err != nil {
-		log.Fatal("could not migrate db")
-	}
+	// Initial Migration Table
+	migration.RunMigration()
 
-	r := Repository{
-		DB: db,
-	}
 	app := fiber.New()
-	r.SetupRoutes(app)
+	// Initial API
+	router.APIGroup(app)
+
+	//port
 	app.Listen(":8080")
 }
