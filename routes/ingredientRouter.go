17c40beeb397d6075d7c5f2dc2976b8101d2a47c
RioIglesias <rioiglesias03@gmail.com>
2024-01-12 22:59:50 +0700

    ingredientRoute

---
+++ routes/ingredientRouter.go
@@ -0,0 +1,12 @@
+package routes
+
+import (
+	"ManganKu_BE/controllers"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func IngredientRoutes(group fiber.Router, repo *controllers.Repository) {
+	// Rute for create recipes
+	group.Get("/get-ingredients", repo.GetIngredients)
+}
