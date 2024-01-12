7e0319105f85aacf2dd49d223dd50cfad1ae23c4
RioIglesias <rioiglesias03@gmail.com>
2024-01-12 09:56:05 +0700

    router Auth for API

---
+++ routes/authRouter.go
@@ -0,0 +1,18 @@
+package routes
+
+import (
+	"ManganKu_BE/controllers"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func AuthRoutes(group fiber.Router, repo *controllers.Repository) {
+	// Rute for user signup
+	group.Post("/signup", repo.SignUpUser)
+
+	// Rute for user login
+	group.Post("/login", repo.SignInUser)
+
+	// Rute for logout
+	group.Post("/logout", repo.LogoutUser)
+}
