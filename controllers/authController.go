06b64bab5b2d994e290c84f53d9d2fcab569ae8a
RioIglesias <rioiglesias03@gmail.com>
2024-01-12 08:42:19 +0700

    authController with token

---
+++ controllers/authController.go
@@ -0,0 +1,107 @@
+package controllers
+
+import (
+	"ManganKu_BE/database"
+	"ManganKu_BE/models"
+	"fmt"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/golang-jwt/jwt"
+	"golang.org/x/crypto/bcrypt"
+)
+
+var SECRET_KEY string = os.Getenv("SECRET_KEY")
+
+func (r *Repository) SignUpUser(c *fiber.Ctx) error {
+	var payload *models.SignUpInput
+
+	if err := c.BodyParser(&payload); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"status": "fail", "message": err.Error()})
+	}
+
+	errors := models.ValidateStruct(payload)
+	if errors != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"status": "fail", "errors": errors})
+
+	}
+
+	if payload.Password != payload.PasswordConfirm {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"status": "fail", "message": "Passwords do not match"})
+
+	}
+
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(payload.Password), bcrypt.DefaultCost)
+
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"status": "fail", "message": err.Error()})
+	}
+
+	newUser := models.User{
+		Name:     payload.Name,
+		Username: strings.ToLower(payload.Username),
+		Password: string(hashedPassword),
+	}
+
+	result := database.DB.Create(&newUser)
+
+	if result.Error != nil && strings.Contains(result.Error.Error(), "duplicate key value violates unique") {
+		return c.Status(fiber.StatusConflict).JSON(fiber.Map{"status": "fail", "message": "User with that email already exists"})
+	} else if result.Error != nil {
+		return c.Status(fiber.StatusBadGateway).JSON(fiber.Map{"status": "error", "message": "Something bad happened"})
+	}
+
+	return c.Status(fiber.StatusCreated).JSON(fiber.Map{"status": "success", "data": fiber.Map{"user": models.FilterUserRecord(&newUser)}})
+}
+
+func (r *Repository) SignInUser(c *fiber.Ctx) error {
+	var payload *models.SignInInput
+
+	if err := c.BodyParser(&payload); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"status": "fail", "message": err.Error()})
+	}
+
+	errors := models.ValidateStruct(payload)
+	if errors != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(errors)
+
+	}
+
+	var user models.User
+	result := database.DB.First(&user, "username = ?", strings.ToLower(payload.Username))
+	if result.Error != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"status": "fail", "message": "Invalid username or Password"})
+	}
+
+	err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(payload.Password))
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"status": "fail", "message": "Invalid username or Password"})
+	}
+
+	tokenByte := jwt.New(jwt.SigningMethodHS256)
+
+	now := time.Now().UTC()
+	claims := tokenByte.Claims.(jwt.MapClaims)
+
+	claims["sub"] = user.ID
+	claims["iat"] = now.Unix()
+	claims["nbf"] = now.Unix()
+
+	tokenString, err := tokenByte.SignedString([]byte(SECRET_KEY))
+
+	if err != nil {
+		return c.Status(fiber.StatusBadGateway).JSON(fiber.Map{"status": "fail", "message": fmt.Sprintf("generating JWT Token failed: %v", err)})
+	}
+
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{"status": "success", "token": tokenString})
+}
+
+func (r *Repository) LogoutUser(c *fiber.Ctx) error {
+	c.Cookie(&fiber.Cookie{
+		Name:  "token",
+		Value: "",
+	})
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{"status": "success"})
+}
