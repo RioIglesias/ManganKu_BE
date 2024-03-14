25fb302f7a297f3d8e302ed113c9686296724816
RioIglesias <rioiglesias03@gmail.com>
2024-03-14 16:54:18 +0700

    route API recipes

--- routes/recipeRouter.go
+++ routes/recipeRouter.go
@@ -9,10 +9,14 @@ import (
 func RecipeRoutes(group fiber.Router, repo *controllers.Repository) {
 	// Rute for create recipes
 	group.Post("/create-recipes", repo.CreateRecipe)
-
 	// Rute for logout
-	group.Post("/create-nutrition", repo.CreateNutrition)
+	// group.Post("/create-nutrition", repo.CreateNutrition)
 	// Rute for
 	group.Post("/create-ingredients", repo.CreateIngredients)
-	group.Post("/get-ingredients", repo.CreateIngredients)
+	group.Get("/get-ingredients", repo.GetIngredientsPerPage)
+	group.Get("/get-recipes", repo.GetRecipesPerPage)
+	group.Post("/upload", repo.UploadFile)
+	group.Get("/storage/recipes/images/thumbnail/:id.png", repo.GetRecipeThubmnailImage)
+	group.Get("/storage/recipes/images/direction-cook/:id", repo.GetRecipesDirectionCookImage)
+	// group.Get("/mainphotos", repo.GetRecipesPerPage)
 }
