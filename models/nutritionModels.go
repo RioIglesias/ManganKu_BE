67ec2264d69d4230d6f415dc49a0344de2ec00ce
RioIglesias <rioiglesias03@gmail.com>
2024-03-14 16:52:19 +0700

    nutrition models

---
+++ models/nutritionModels.go
@@ -0,0 +1,14 @@
+package models
+
+// type Nutrition struct {
+// 	ID    uint   `gorm:"primary_key"`
+// 	Name  string `gorm:"type:varchar;uniqueIndex;not null"`
+// Image string
+// 	Value int    `gorm:"type:int;not null"`
+// }
+
+// type CreateNutrition struct {
+// 	Name  string `json:"name"`
+// 	Image string `json:"image"`
+// 	Value int    `json:"value"`
+// }
