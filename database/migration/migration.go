3054f5942516bf7b7e993401d857eef83df4dd91
RioIglesias <rioiglesias03@gmail.com>
2024-03-14 16:50:39 +0700

    migration UserDetails

--- database/migration/migration.go
+++ database/migration/migration.go
@@ -7,8 +7,9 @@ import (
 
 func RunMigration() {
 	database.DB.AutoMigrate(&models.User{})
+	database.DB.AutoMigrate(&models.UserDetail{})
 	database.DB.AutoMigrate(&models.Recipe{})
 	database.DB.AutoMigrate(&models.DirectionCook{})
 	database.DB.AutoMigrate(&models.Ingredient{})
-	database.DB.AutoMigrate(&models.Nutrition{})
+	// database.DB.AutoMigrate(&models.Nutrition{})
 }
