f767d1469f0bd2f55215daa894858a12f45c7126
RioIglesias <rioiglesias03@gmail.com>
2024-03-31 17:31:45 +0700

    add category table

--- database/migration/migration.go
+++ database/migration/migration.go
@@ -7,9 +7,10 @@ import (
 
 func RunMigration() {
 	database.DB.AutoMigrate(&models.User{})
-	database.DB.AutoMigrate(&models.UserDetail{})
 	database.DB.AutoMigrate(&models.Recipe{})
+	// database.DB.AutoMigrate(&models.Favorite{})
 	database.DB.AutoMigrate(&models.DirectionCook{})
 	database.DB.AutoMigrate(&models.Ingredient{})
+	database.DB.AutoMigrate(&models.Category{})
 	// database.DB.AutoMigrate(&models.Nutrition{})
 }
