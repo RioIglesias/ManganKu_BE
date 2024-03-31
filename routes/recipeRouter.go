8ebc36ac6a883821f2e46993bef84d7d3d970aa0
RioIglesias <rioiglesias03@gmail.com>
2024-03-31 17:33:07 +0700

    refactor

--- routes/recipeRouter.go
+++ routes/recipeRouter.go
@@ -9,14 +9,14 @@ import (
 func RecipeRoutes(group fiber.Router, repo *controllers.Repository) {
 	// Rute for create recipes
 	group.Post("/create-recipes", repo.CreateRecipe)
-	// Rute for logout
+
+	// Rute for create nutrition
 	// group.Post("/create-nutrition", repo.CreateNutrition)
-	// Rute for
-	group.Post("/create-ingredients", repo.CreateIngredients)
-	group.Get("/get-ingredients", repo.GetIngredientsPerPage)
+
+	// Rute for get recipes
 	group.Get("/get-recipes", repo.GetRecipesPerPage)
-	group.Post("/upload", repo.UploadFile)
+
+	// Rute for get image by url
 	group.Get("/storage/recipes/images/thumbnail/:id.png", repo.GetRecipeThubmnailImage)
 	group.Get("/storage/recipes/images/direction-cook/:id", repo.GetRecipesDirectionCookImage)
-	// group.Get("/mainphotos", repo.GetRecipesPerPage)
 }
