9a1ab6ffef40ae19b430bd5bf3c2934565055855
RioIglesias <rioiglesias03@gmail.com>
2024-04-04 19:35:36 +0700

    add filename for image

--- models/ingredientsModels.go
+++ models/ingredientsModels.go
@@ -1,14 +1,13 @@
 package models
 
 type Ingredient struct {
-	ID    uint   `gorm:"primary_key"`
-	Name  string `gorm:"type:varchar;not null;unique"`
-	Image string
-	Value float32 `gorm:"type:float;not null"`
+	ID            uint   `gorm:"primary_key"`
+	Name          string `gorm:"type:varchar;not null;unique"`
+	Image         string
+	FileNameImage string `gorm:"uniqueIndex" json:"-"`
 }
 
 type CreateIngredient struct {
-	Name  string  `json:"name"`
-	Image string  `json:"image"`
-	Value float32 `json:"value"`
+	Name  string `json:"name"`
+	Image string `json:"image"`
 }
