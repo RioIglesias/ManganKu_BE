bb47d893581047ab0f559fb48d42d01e812f7bdc
RioIglesias <rioiglesias03@gmail.com>
2024-04-04 19:34:41 +0700

    migration ingredientlist

--- database/migration/migration.go
+++ database/migration/migration.go
@@ -10,6 +10,7 @@ func RunMigration() {
 	database.DB.AutoMigrate(&models.Recipe{})
 	// database.DB.AutoMigrate(&models.Favorite{})
 	database.DB.AutoMigrate(&models.DirectionCook{})
+	database.DB.AutoMigrate(&models.IngredientList{})
 	database.DB.AutoMigrate(&models.Ingredient{})
 	database.DB.AutoMigrate(&models.Category{})
 	// database.DB.AutoMigrate(&models.Nutrition{})
