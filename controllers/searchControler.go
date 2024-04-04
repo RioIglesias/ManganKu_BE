076e013cc6fcd8e770591f7dac951031372e8c53
RioIglesias <rioiglesias03@gmail.com>
2024-04-04 19:33:45 +0700

    refactor search func

--- controllers/searchControler.go
+++ controllers/searchControler.go
@@ -3,62 +3,56 @@ package controllers
 import (
 	"ManganKu_BE/database"
 	"ManganKu_BE/models"
+	"math/rand"
 	"strconv"
 	"strings"
 
 	"github.com/gofiber/fiber/v2"
 )
 
-func (r *Repository) FilterByCategory(c *fiber.Ctx) error {
+func (r *Repository) SearchFunc(c *fiber.Ctx) error {
+	search := c.Query("c", "")
+	searchQuery := c.Query("s", "")
 
-	search := c.Query("filter", "")
-
-	var category []models.Recipe
-	query := database.DB.Preload("Ingredients").Preload("DirectionCooks")
-	if search != "" {
-		query = query.Where("Category = ?", search)
-	}
-	result := query.Find(&category)
-
-	if result.Error != nil {
-		return c.Status(fiber.StatusBadGateway).JSON(fiber.Map{"status": "error", "message": "Something bad happened"})
-	}
-	for i := range category {
-		// Ubah URL gambar utama
-		category[i].MainPhoto = c.BaseURL() + "/storage/category/images/thumbnail/" + strings.ReplaceAll(category[i].CreatedAt, "-", "") + strconv.Itoa(int(category[i].ID)) + ".png"
-
-		// Ubah URL gambar langkah-langkah
-		for j := range category[i].DirectionCooks {
-			category[i].DirectionCooks[j].Image = c.BaseURL() + "/storage/category/images/direction-cook/" + strings.ReplaceAll(category[i].CreatedAt, "-", "") + strconv.Itoa(int(category[i].DirectionCooks[j].ID)) + ".png"
-		}
+	limitstr := c.Query("limit", "10")
+	limit, err := strconv.Atoi(limitstr)
+	if err != nil || limit <= 0 {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"status": "fail", "message": "Invalid per_page value"})
 	}
 
-	return c.Status(fiber.StatusOK).JSON(fiber.Map{"status": "success", "data": fiber.Map{"recipes": category}})
-}
-
-func (r *Repository) SearchByQuery(c *fiber.Ctx) error {
-
-	search := c.Query("search", "")
+	offset := limit
 
 	var recipes []models.Recipe
-	query := database.DB.Preload("Ingredients").Preload("DirectionCooks")
+	query := database.DB.Preload("Ingredients").Preload("DirectionCooks").Preload("Category").Preload("Ingredients.Ingredient")
 	if search != "" {
-		query = query.Where("Name = ?", search)
+		categories := strings.Split(search, ",")
+		query = query.Where("category_id IN (?)", categories)
 	}
-	result := query.Find(&recipes)
+	if searchQuery != "" {
+		query = query.Where("Name ILIKE ?", "%"+searchQuery+"%")
+	}
+	result := query.Offset(offset).Limit(limit).Find(&recipes)
 
-	if result.Error != nil {
-		return c.Status(fiber.StatusBadGateway).JSON(fiber.Map{"status": "error", "message": "Something bad happened"})
-	}
+	rand.Shuffle(len(recipes), func(i, j int) {
+		recipes[i], recipes[j] = recipes[j], recipes[i]
+	})
+
 	for i := range recipes {
 		// Ubah URL gambar utama
-		recipes[i].MainPhoto = c.BaseURL() + "/storage/recipes/images/thumbnail/" + strings.ReplaceAll(recipes[i].CreatedAt, "-", "") + strconv.Itoa(int(recipes[i].ID)) + ".png"
+		if recipes[i].MainPhoto != "" {
+			recipes[i].MainPhoto = c.BaseURL() + "/api/storage/recipes/images/thumbnail/" + recipes[i].MainPhotoName + ".png"
+		}
 
 		// Ubah URL gambar langkah-langkah
 		for j := range recipes[i].DirectionCooks {
-			recipes[i].DirectionCooks[j].Image = c.BaseURL() + "/storage/recipes/images/direction-cook/" + strings.ReplaceAll(recipes[i].CreatedAt, "-", "") + strconv.Itoa(int(recipes[i].DirectionCooks[j].ID)) + ".png"
+			if recipes[i].DirectionCooks[j].Image != "" {
+				recipes[i].DirectionCooks[j].Image = c.BaseURL() + "/api/storage/recipes/images/direction-cook/" + recipes[i].DirectionCooks[j].ImageName + ".png"
+			}
 		}
 	}
 
-	return c.Status(fiber.StatusOK).JSON(fiber.Map{"status": "success", "data": fiber.Map{"recipes": recipes}})
+	if result.Error != nil {
+		return c.Status(fiber.StatusBadGateway).JSON(fiber.Map{"status": "error", "message": "Something bad happened"})
+	}
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{"status": "success", "data": fiber.Map{"recipes": models.FilterRecipeRecordList(recipes)}})
 }
